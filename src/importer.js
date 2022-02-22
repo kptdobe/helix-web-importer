0ffe93296331d41a0ef2a44afcbe7c2d2fd62ac1
Alexandre Capt <acapt@adobe.com>
2022-02-22 14:23:45 +0100

    fix: UMD broken on some sites

--- src/importer.js
+++ src/importer.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import path, { dirname } from 'path';
+import path from 'path';
 import {
   PageImporter,
   PageImporterResource,
